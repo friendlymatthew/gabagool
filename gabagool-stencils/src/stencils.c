61a6f75ac27b318a8d1356165104387ff12623ff
Matthew Kim <38759997+friendlymatthew@users.noreply.github.com>
2026-03-31 22:00:56 -0400

    write i32 cmp stencils

--- gabagool-stencils/src/stencils.c
+++ gabagool-stencils/src/stencils.c
@@ -62,4 +62,53 @@ I32_BINOP(i32_add, +)
 I32_BINOP(i32_sub, -)
 I32_BINOP(i32_mul, *)
 
+#define I32_CMP(name, op)                                                      \
+  void name(StencilContext *ctx) {                                             \
+    ctx->stack_pointer -= 1;                                                   \
+                                                                               \
+    int32_t b = (int32_t)ctx->stack[ctx->stack_pointer];                       \
+    int32_t a = (int32_t)ctx->stack[ctx->stack_pointer - 1];                   \
+                                                                               \
+    ctx->stack[ctx->stack_pointer - 1] = (a op b) ? 1 : 0;                     \
+    ctx->pc += 1;                                                              \
+                                                                               \
+    CHECK_SNAPSHOT(ctx);                                                       \
+    __attribute__((musttail)) return ctx->fn_table[ctx->pc](ctx);              \
+  }
+
+I32_CMP(i32_lt_signed, <)
+I32_CMP(i32_gt_signed, >)
+I32_CMP(i32_le_signed, <=)
+I32_CMP(i32_ge_signed, >=)
+I32_CMP(i32_eq, ==)
+I32_CMP(i32_ne, !=)
+
+#define U32_CMP(name, op)                                                      \
+  void name(StencilContext *ctx) {                                             \
+    ctx->stack_pointer -= 1;                                                   \
+                                                                               \
+    uint32_t b = (uint32_t)ctx->stack[ctx->stack_pointer];                     \
+    uint32_t a = (uint32_t)ctx->stack[ctx->stack_pointer - 1];                 \
+                                                                               \
+    ctx->stack[ctx->stack_pointer - 1] = (a op b) ? 1 : 0;                     \
+    ctx->pc += 1;                                                              \
+                                                                               \
+    CHECK_SNAPSHOT(ctx);                                                       \
+    __attribute__((musttail)) return ctx->fn_table[ctx->pc](ctx);              \
+  }
+
+U32_CMP(i32_lt_unsigned, <)
+U32_CMP(i32_gt_unsigned, >)
+U32_CMP(i32_le_unsigned, <=)
+U32_CMP(i32_ge_unsigned, >=)
+
+void i32_eq_zero(StencilContext *ctx) {
+  uint32_t a = (uint32_t)ctx->stack[ctx->stack_pointer - 1];
+  ctx->stack[ctx->stack_pointer - 1] = (a == 0) ? 1 : 0;
+  ctx->pc += 1;
+
+  CHECK_SNAPSHOT(ctx);
+  __attribute__((musttail)) return ctx->fn_table[ctx->pc](ctx);
+}
+
 void return_(StencilContext *ctx) { ctx->exit_reason = EXIT_RETURN; }
